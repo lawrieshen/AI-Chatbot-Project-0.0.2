84f1dd76c9b19afedc39488f0565e81ea13daa15
Lawrence Shen <lawrenceshen1213@gmail.com>
2023-12-20 17:09:00 +1100

    Simulate Short-term Memory for the AI Model

--- worker/src/schema/chat.py
+++ worker/src/schema/chat.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from pydantic import BaseModel, Field
-from typing import List, Optional
+from typing import Optional, List
 import uuid
 
 
@@ -8,10 +8,3 @@ class Message(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     msg: str
     timestamp: str = Field(default_factory=lambda: str(datetime.now()))
-
-
-class Chat(BaseModel):
-    token: str
-    messages: List[Message]
-    name: str
-    session_start: str = Field(default_factory=lambda: str(datetime.now()))
