24041facf90c3eada593b25524db2d6e0f1a0195
Lawrence Shen <lawrenceshen1213@gmail.com>
2023-12-21 16:35:42 +1100

    tttttt "Simulate Short-term Memory for the AI Model"
    This reverts commit 84f1dd76c9b19afedc39488f0565e81ea13daa15.

--- worker/main.py
+++ worker/main.py
@@ -1,42 +1,11 @@
 from src.redis.config import Redis
 import asyncio
-from src.model.inferenceAPI import BlenderBotInferenceAPI
-from src.redis.cache import Cache
-from src.schema.chat import Message
-
-redis = Redis()
-
 
 async def main():
-    json_client = redis.create_rejson_connection()
-    redis_client = await redis.create_connection()
-
-    await Cache(json_client).add_message_to_cache(token="f000c5a9-7ec9-4e66-8780-bc9d5b12fd25", source="human",
-                                                  message_data={
-                                                      "id": "3",
-                                                      "msg": "How to survive as a Software Engineering student with "
-                                                             "AI prevailing?",
-                                                      "timestamp": "2022-07-16 13:20:01.092109"
-                                                  })
-
-    data = await Cache(json_client).get_chat_history(token="f000c5a9-7ec9-4e66-8780-bc9d5b12fd25")
-    print(data)
-
-    message_data = data['messages'][-4:]
-
-    input = ["" + i['msg'] for i in message_data]
-    input = " ".join(input)
-
-    res = BlenderBotInferenceAPI().query(input=input)
-
-    msg = Message(
-        msg=res
-    )
-
-    print(msg)
-    await Cache(json_client).add_message_to_cache(token="f000c5a9-7ec9-4e66-8780-bc9d5b12fd25", source='bot',
-                                                  message_data=msg.model_dump())
-
+    redis = Redis()
+    redis = await redis.create_connection()
+    print(redis)
+    await redis.set("key", "value")
 
 if __name__ == "__main__":
     asyncio.run(main())
--- worker/src/redis/config.py
+++ worker/src/redis/config.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 import aioredis
-from rejson import Client
 
 load_dotenv()
 
@@ -12,8 +11,6 @@ class Redis():
         self.REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
         self.REDIS_USER = os.environ['REDIS_USER']
         self.connection_url = f"redis://{self.REDIS_USER}:{self.REDIS_PASSWORD}@{self.REDIS_URL}"
-        self.REDIS_HOST = os.environ['REDIS_HOST']
-        self.REDIS_PORT = os.environ['REDIS_PORT']
 
     async def create_connection(self):
         self.connection = aioredis.from_url(
@@ -21,11 +18,3 @@ class Redis():
         )
 
         return self.connection
-
-    def create_rejson_connection(self):
-        self.redisJson = Client(host=self.REDIS_HOST,
-                                port=self.REDIS_PORT,
-                                decode_responses=True,
-                                username=self.REDIS_USER,
-                                password=self.REDIS_PASSWORD)
-        return self.redisJson
